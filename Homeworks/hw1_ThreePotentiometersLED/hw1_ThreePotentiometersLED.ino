ffce35146bf352f50adb9c70fa0732baef965266
Radu Dilirici <diliriciradu@gmail.com>
2019-11-05 21:38:02 +0200

    Update hw1_ThreePotentiometersLED.ino

--- Homeworks/hw1_ThreePotentiometersLED/hw1_ThreePotentiometersLED.ino
+++ Homeworks/hw1_ThreePotentiometersLED/hw1_ThreePotentiometersLED.ino
@@ -15,6 +15,12 @@ void setup() {
   pinMode(BLUE_PIN, OUTPUT);
 }
 
+void setColor(int red, int green, int blue) {
+  analogWrite(RED_PIN, red);
+  analogWrite(GREEN_PIN, green);
+  analogWrite(BLUE_PIN, blue);
+}
+
 void loop() {
   int sensorValueR = analogRead(POT_R);
   int sensorValueG = analogRead(POT_G);
@@ -22,7 +28,5 @@ void loop() {
   int redVal = map(sensorValueR, 0, 1023, 0, 255);
   int greenVal = map(sensorValueG, 0, 1023, 0, 255);
   int blueVal = map(sensorValueB, 0, 1023, 0, 255);
-  analogWrite(RED_PIN, redVal);
-  analogWrite(GREEN_PIN, greenVal);
-  analogWrite(BLUE_PIN, blueVal);
+  setColor(redVal, greenVal, blueVal);
 }
