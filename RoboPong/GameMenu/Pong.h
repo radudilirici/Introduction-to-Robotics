bf469afa20ba96932b1720bda3a022f4f1615bae
Radu Dilirici <diliriciradu@gmail.com>
2020-02-24 16:54:50 +0200

    Edited comment

--- RoboPong/GameMenu/Pong.h
+++ RoboPong/GameMenu/Pong.h
@@ -4,7 +4,7 @@
 bool whosPlaying = 0;  // 0 = MrRoboto, 1 = Bluetooth
 byte winScore = 11;
 
-bool inSubMenu, justEnteredMenu;  // these two are maily used in the Game Menu, but I need them in Pong for Game Over so the game can return to menu
+bool inSubMenu, justEnteredMenu;  // these two are mainly used in the Game Menu, but I need them in Pong for Game Over so the game can return to menu
 
 void restartPong();
 void iteratePong();
